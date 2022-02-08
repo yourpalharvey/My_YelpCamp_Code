795f8043f26ef8d9e9fdaef53f48e64319f1dd20
Harvey Beynon <harveyibeynon@gmail.com>
2022-02-08 17:31:55 +0000

    Added show page - camp details

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-
 app.get('/', (req, res) => {
     res.render('home')
 })
@@ -27,6 +26,11 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds})
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground })
 })
 
 app.listen(3000, () => {
