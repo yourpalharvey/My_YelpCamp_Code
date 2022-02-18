bb268894c8cb3827112875bfe67e7cda6d25e48b
Harvey Beynon <harveyibeynon@gmail.com>
2022-02-18 16:54:50 +0000

    Added basic error handling for new campground form

--- app.js
+++ app.js
@@ -38,10 +38,14 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
+app.post('/campgrounds', async (req, res, next) => {
+    try {
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`);
+    } catch (e) {
+        next(e);
+    }
 });
 
 app.get('/campgrounds/:id', async (req, res) => {
@@ -64,6 +68,10 @@ app.delete('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+});
+
+app.use((err, req, res, next) => {
+    res.send('Somthing When Wrong')
 });
 
 app.listen(3000, () => {
