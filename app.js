e4834e3bb53d2510dc8400e8d3f645049141f24f
Harvey Beynon <harveyibeynon@gmail.com>
2022-02-20 22:54:53 +0000

    Can now delete reviews from the show page

--- app.js
+++ app.js
@@ -109,6 +109,14 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
+// path to delete an existing review - uses mongo PULL operator
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}));
+
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
 })
