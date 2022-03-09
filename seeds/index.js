37f07b3fba335f6aa2caa140c1e37cb890dd352f
Harvey Beynon <harveyibeynon@gmail.com>
2022-03-09 12:38:50 +0000

    Added author property to campground

--- seeds/index.js
+++ seeds/index.js
@@ -26,6 +26,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: '6227689e038dea6d7cf78d56',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image:
