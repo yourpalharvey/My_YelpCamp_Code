e143a06da125b4a6d4fe25b5694837cead505971
Harvey Beynon <harveyibeynon@gmail.com>
2022-03-09 13:32:01 +0000

    Set up review premissions

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utils/catchAsync');
-const { validateReview } = require('../middleware');
+const { isLoggedIn, validateReview } = require('../middleware');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 
 // Request to added new review
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   const review = new Review(req.body.review);
+  review.author = req.user._id;
   campground.reviews.push(review);
   await review.save();
   await campground.save();
