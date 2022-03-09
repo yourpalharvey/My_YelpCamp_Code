5520f676a6ca0cf0f7d5d0bdd1ae95ed1ea2ce3c
Harvey Beynon <harveyibeynon@gmail.com>
2022-03-09 12:57:36 +0000

    Added campground edit premissions

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -64,7 +64,12 @@ router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
 // Updates the campground details with the new info from the edit form
 router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have premission to do that!');
+        return res.redirect(`/campgrounds/${ id }`);
+    }
+    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash('success', 'Successfully updated campground!')
     res.redirect(`/campgrounds/${campground._id}`);
 }));
