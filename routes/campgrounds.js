1be585ac922be9d42242c385dca35a0d715d10e3
Harvey Beynon <harveyibeynon@gmail.com>
2022-03-10 16:43:31 +0000

    Upload images to Cloudinary

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,7 +4,8 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware.js');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
