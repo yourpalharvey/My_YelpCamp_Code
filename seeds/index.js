7ff2c7b49450232581c50302ac04dca2cb06213b
Harvey Beynon <harveyibeynon@gmail.com>
2022-03-18 19:25:45 +0000

    Set up method to seed atlus database

--- seeds/index.js
+++ seeds/index.js
@@ -3,9 +3,11 @@ const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
 
-mongoose.connect("mongodb://localhost:27017/my-yelp-camp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/my-yelp-camp';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
